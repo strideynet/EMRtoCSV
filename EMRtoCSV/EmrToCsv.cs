f4340e7d8da8b17916434ad15ac348f9ec6a7cdc
Unknown <noah.stride@selfsown.com>
2018-09-15 21:34:28 +0100

    Init

---
+++ EMRtoCSV/EmrToCsv.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Seca.EmrContract;
+using Seca.EmrContract.Measurements;
+
+namespace EMRtoCSV
+{
+    [EmrModule("EMRtoCSV", "Sends measurements to a CSV file | Noah Stride | 1.01", null)]
+    public class EMRtoCSV: Seca.EmrContract.Module
+    {
+        private string path = @"c:\secadata\data.csv";
+        
+        public override void SendMeasurement(IMeasurement measurement)
+        {
+            string height = measurement.HeightMeasurement.ToString(".", LengthUnit.Meter);
+            string weight = measurement.WeightMeasurement.ToString(".", MassUnit.Kilograms);
+
+            string time = DateTime.Now.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss.fffffffK");
+            
+            using (StreamWriter w = File.AppendText(path))
+            {
+                w.WriteLine(time + "," + weight + "," + height);
+            }
+
+        }
+    }
+}
